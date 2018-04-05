b1750a4972832272543483e8175ee9354891b20f
Daniel <danielmorillo249@hotmail.com>
2018-04-05 12:42:13 +0200

    added orderbook for each resource

--- market.cs
+++ market.cs
@@ -1,14 +1,17 @@
 
 class Market {
-    OrderBook Orderbook;
+    OrderBook[] Orderbooks;
 
-    public void placeOffer(int ID, int Ammount, int Price) {
-        Offer newOffer = new Offer(ID, Ammount, Price);
-        Orderbook.addOffer(newOffer);
+    public void placeOffer(int sellerID, int resourceID, int Ammount, int Price) {
+        Offer newOffer = new Offer(sellerID, Ammount, Price);
+        Orderbooks[resourceID].addOffer(newOffer);
     }
 
     // constructor
-    public Market() {
-        Orderbook = new OrderBook();
+    public Market(int resourceAmmount) {
+        Orderbooks = new OrderBook[resourceAmmount];
+        for(int i = 0; i < resourceAmmount; i++) {
+            Orderbooks[i] = new OrderBook();
+        }
     }
 }
