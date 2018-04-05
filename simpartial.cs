68fa314089d98cc7ed7d0f3700c2ad60e50db30b
Daniel <danielmorillo249@hotmail.com>
2018-04-05 12:41:47 +0200

    moved extra simulation functionality to partial class

---
+++ simpartial.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Linq;
+
+partial class Simulation {
+
+    // extra functionality to specify the ammount of each profession, 
+    //instead of same for every profession
+
+    // constructor
+    public Simulation(params int[] ammountPerProf) {
+        // get professions from enum at compile
+        profAmmount = Enum.GetNames(typeof(Profession)).Length;
+        // add inputs and initialize array
+        Population = new Person[ammountPerProf.Sum()];
+        // use inputs to initilize population
+        initPopulation(ammountPerProf);
+    }
+
+
+    void initPopulation(int[] ammountPerProf) {
+        if(ammountPerProf.Length < profAmmount) {
+            Console.WriteLine("Error: not enough inputs");
+        } else {
+            int counter = 0;
+            // for each profession
+            for(int i = 0; i < profAmmount; i++) {
+                Type type = null;
+                try {
+                    // cast the current proffesion
+                    Profession prof = (Profession)i;
+                    // cast it to type
+                    type = Type.GetType(prof.ToString());
+                } catch(Exception e) {
+                    Console.WriteLine("Error: Could not cast Profession");
+                    Console.WriteLine("Log: Happened in {0}-ava Profession",i);
+                    Console.WriteLine(e);
+                }
+
+                // add to Population
+                for(int j = 0; j < ammountPerProf[i]; j++) {
+                    try {
+                        var temp = Activator.CreateInstance(type);
+                        Population[counter] = (Person)temp;
+                    } catch (Exception e) {
+                        Console.WriteLine("Error: Could not initialize Person from type");
+                        Console.WriteLine(e);
+                    }
+                    counter++;
+                }
+            }
+        }
+    }
+
+
+}
--- simulation.cs
+++ simulation.cs
@@ -1,8 +1,8 @@
 using System;
-using System.Linq;
+// using System.Linq;
 using System.Diagnostics;
 
-class Simulation {
+partial class Simulation {
 
     // number of professions
     public int profAmmount { get; private set; }
@@ -12,95 +12,50 @@ class Simulation {
 
 
     // initialize Persons in array
-    void initPopulation(int[] ammountPerProf) {
-        if(ammountPerProf.Length < profAmmount) {
-            Console.WriteLine("Error: not enough inputs");
-        } else {
-            int counter = 0;
-            // for each profession
-            for(int i = 0; i < profAmmount; i++) {
-                Type type = null;
-                try {
-                    // cast the current proffesion
-                    Profession prof = (Profession)i;
-                    // cast it to type
-                    type = Type.GetType(prof.ToString());
-                } catch(Exception e) {
-                    Console.WriteLine("Error: Could not cast Profession");
-                    Console.WriteLine("Log: Happened in {0}-ava Profession",i);
-                    Console.WriteLine(e);
-                }
-
-                // add to Population
-                for(int j = 0; j < ammountPerProf[i]; j++) {
-                    try {
-                        var temp = Activator.CreateInstance(type);
-                        Population[counter] = (Person)temp;
-                    } catch (Exception e) {
-                        Console.WriteLine("Error: Could not initialize Person from type");
-                        Console.WriteLine(e);
-                    }
-                    counter++;
-                }
-            }
-        }
-    }
-
     void initPopulation(int ammountPerProf) {
         int counter = 0;
 
         for(int i = 0; i < profAmmount; i++) {
-            Type type = null;
+            initProfession(i, ammountPerProf, counter);
+            counter += ammountPerProf;
+        }
+    }
+
+    void initProfession(int professionID, int Ammount, int startPoint) {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        Type type = null;
             try {
                 // cast the current proffesion
-                Profession prof = (Profession)i;
+                Profession prof = (Profession)professionID;
                 // cast it to type
                 type = Type.GetType(prof.ToString());
             } catch(Exception e) {
                 Console.WriteLine("Error: Could not cast Profession");
-                Console.WriteLine("Log: Happened in {0}-ava Profession",i);
+                Console.WriteLine("Log: Happened in {0}-ava Profession", ((Profession)professionID).ToString());
                 Console.WriteLine(e);
             }
 
-            for(int j = 0; j < ammountPerProf; j++) {
+            for(int j = 0; j < Ammount; j++) {
                 try {
                     var temp = Activator.CreateInstance(type);
-                    Population[counter] = (Person)temp;
+                    Population[startPoint + j] = (Person)temp;
                 } catch (Exception e) {
                     Console.WriteLine("Error: Could not initialize Person from type");
                     Console.WriteLine(e);
                 }
-                counter++;
             }
 
             // test
             Console.WriteLine(type.ToString() + "\t" + stopwatch.Elapsed.TotalMilliseconds.ToString());
             stopwatch.Restart();
-
-        }
     }
 
-
-    // constructor
-    public Simulation(params int[] ammountPerProf) {
-        // get professions from enum at compile
-        profAmmount = Enum.GetNames(typeof(Profession)).Length;
-        // add inputs and initialize array
-        Population = new Person[ammountPerProf.Sum()];
-        // use inputs to initilize population
-        initPopulation(ammountPerProf);
-    }
-    // test
-    Stopwatch stopwatch;
     public Simulation(int ammountPerProf) {
         // get professions from enum at compile
         profAmmount = Enum.GetNames(typeof(Profession)).Length;
         
         Population = new Person[ammountPerProf*profAmmount];
-        stopwatch = new Stopwatch();
-        
-        // test
-        stopwatch.Start();
 
         initPopulation(ammountPerProf);
 
