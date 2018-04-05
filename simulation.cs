cbd5b502bed79c23f4487cd1d14362a0a903eef6
Daniel <danielmorillo249@hotmail.com>
2018-04-05 14:18:10 +0200

    generalized unit initialization and unified partial simulation class

--- simulation.cs
+++ simulation.cs
@@ -2,63 +2,89 @@ using System;
 // using System.Linq;
 using System.Diagnostics;
 
-partial class Simulation {
+class Simulation {
 
     // number of professions
     public int profAmmount { get; private set; }
 
     // references to Persons
-    public Person[] Population { get; private set; }
+    public Person[][] Population { get; private set; }
+
 
 
     // initialize Persons in array
     void initPopulation(int ammountPerProf) {
-        int counter = 0;
-
         for(int i = 0; i < profAmmount; i++) {
-            initProfession(i, ammountPerProf, counter);
-            counter += ammountPerProf;
+            initProfession(i, ammountPerProf);
         }
     }
 
-    void initProfession(int professionID, int Ammount, int startPoint) {
+
+
+    void initProfession(int professionID, int Ammount) {
         Stopwatch stopwatch = new Stopwatch();
         stopwatch.Start();
         Type type = null;
+        try {
+            // cast the current proffesion
+            Profession prof = (Profession)professionID;
+            // cast it to type
+            type = Type.GetType(prof.ToString());
+        } catch (Exception e) {
+            Console.WriteLine("Error: Could not cast Profession");
+            Console.WriteLine("Log: Happened in {0}-ava Profession", ((Profession)professionID).ToString());
+            Console.WriteLine(e);
+        }
+
+        Person[] newDemographic = new Person[Ammount];
+
+        for(int j = 0; j < Ammount; j++) {
             try {
-                // cast the current proffesion
-                Profession prof = (Profession)professionID;
-                // cast it to type
-                type = Type.GetType(prof.ToString());
-            } catch(Exception e) {
-                Console.WriteLine("Error: Could not cast Profession");
-                Console.WriteLine("Log: Happened in {0}-ava Profession", ((Profession)professionID).ToString());
+                var temp = Activator.CreateInstance(type);
+                newDemographic[j] = (Person)temp;
+            } catch (Exception e) {
+                Console.WriteLine("Error: Could not initialize Person from type");
                 Console.WriteLine(e);
             }
+        }
+        Population[professionID] = newDemographic;
 
-            for(int j = 0; j < Ammount; j++) {
-                try {
-                    var temp = Activator.CreateInstance(type);
-                    Population[startPoint + j] = (Person)temp;
-                } catch (Exception e) {
-                    Console.WriteLine("Error: Could not initialize Person from type");
-                    Console.WriteLine(e);
-                }
-            }
-
-            // test
-            Console.WriteLine(type.ToString() + "\t" + stopwatch.Elapsed.TotalMilliseconds.ToString());
-            stopwatch.Restart();
+        // test
+        Console.WriteLine(type.ToString() + "\t" + stopwatch.Elapsed.TotalMilliseconds.ToString());
     }
 
     public Simulation(int ammountPerProf) {
         // get professions from enum at compile
         profAmmount = Enum.GetNames(typeof(Profession)).Length;
         
-        Population = new Person[ammountPerProf*profAmmount];
+        Population = new Person[profAmmount][];
 
         initPopulation(ammountPerProf);
+    }
 
 
+
+    // extra functionality to specify the ammount of each profession, 
+    //instead of same for every profession
+
+    // constructor
+    public Simulation(params int[] ammountPerProf) {
+        // get professions from enum at compile
+        profAmmount = Enum.GetNames(typeof(Profession)).Length;
+        // add inputs and initialize array
+        Population = new Person[profAmmount][];
+        // use inputs to initilize population
+        initPopulation(ammountPerProf);
+    }
+
+    // initialize Persons in array
+    void initPopulation(int[] ammountPerProf) {
+        if(ammountPerProf.Length < profAmmount) {
+            Console.WriteLine("Error: not enough inputs");
+        } else {
+            for(int i = 0; i < profAmmount; i++) {
+                initProfession(i, ammountPerProf[i]);
+            }
+        }
     }
 }
