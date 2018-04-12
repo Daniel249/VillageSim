f2dd555181bd16d3f4f247d8e77e3393b35d2c58
Daniel <danielmorillo249@hotmail.com>
2018-04-12 15:30:11 +0200

    generic sell and buy for all persons

--- person.cs
+++ person.cs
@@ -2,6 +2,7 @@ using System;
 
 abstract class Person {
 
+    protected static Random rnd = new Random();
     // profession
     // main product location in inventory
     public abstract Profession Role { get; }
@@ -26,6 +27,7 @@ abstract class Person {
         work();
         considerSell();
         considerBuy();
+        checkBankrupcy();
     }
 
     // override by inherited classes
@@ -33,22 +35,53 @@ abstract class Person {
     
 
     // sell logic
-    void considerSell() {
-        // check inventory and role. think and then sell
+    // always sells produced
+    protected virtual void considerSell() {
+        Market resourceMarket = Simulation.SimInstance.Markets[(int)Role];
+        decimal resourcePrice = resourceMarket.getLastPrice();
+        int randomNum = rnd.Next(-3, 4);
+        decimal randomPrice = decimal.Divide(randomNum, 20) + resourcePrice;
+        if(randomPrice < 0.1m) {
+            randomPrice = 0.1m;
+        }
+        resourceMarket.placeOffer(this, Inventory[(int)Role],randomPrice);
     }
 
     // buy logic
+    // implementation for food
     protected virtual void considerBuy() {
+        Market foodMarket = Simulation.SimInstance.Markets[(int)Profession.Farmer];
+        if(Inventory[(int)Profession.Farmer] < 2) {
+            decimal lastLogPrice = foodMarket.getLastPrice();
 
+            int buyAmmount = (int)(Cash / lastLogPrice);
+            if(buyAmmount > 3) {
+                buyAmmount = 3;
+            }
+            // buy max possible ammount or 3 at 0.1 more than market price
+            foodMarket.searchOffer(this, (int)Profession.Farmer, buyAmmount, lastLogPrice+0.1m);
+        }
     }
 
+    // check for death
+    protected void checkBankrupcy() {
+        if(Inventory[(int)Profession.Farmer] < 0) {
+            Simulation.SimInstance.DeadPeople.Add(this);
+        }
+    }
     // set inventory. on respawn only
-    public void setInventory(int[] _inventory) {
+    public void Respawn(int[] _inventory) {
         Inventory = _inventory;
     }
 
     // constructor
     protected Person() {
         Inventory = new int[Simulation.SimInstance.profAmmount];
+        // gift food and wood to everyone
+        int gift = rnd.Next(3, 7);
+        if(Role != Profession.Farmer) {
+            Inventory[(int)Profession.Farmer] = gift;
+        }
+        Inventory[(int)Profession.Lumberjack] = gift;
     }
 }
